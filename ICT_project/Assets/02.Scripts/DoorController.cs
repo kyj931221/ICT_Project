4112ee65fb5748f46a6439c1fa84b8ae906effa2
KimYoungJun <kyj931221@gmail.com>
2024-08-20 11:40:22 +0900

    Asset 폴더 정리 및 장소 텔레포트 기능 추가

---
+++ ICT_project/Assets/02.Scripts/DoorController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            OpenDoor();
+        }
+    }
+
+    public void OpenDoor()
+    {
+        door.SetActive(false);
+    }
+}
---
+++ ICT_project/Assets/02.Scripts/Teleporting.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporting : MonoBehaviour
+{
+    // 특정 오브젝트의 위치값을 입력받아서 이동한다.
+    public Transform Taget;
+    public GameObject Game_Player;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator waitTel()
+    {
+        Debug.Log("텔포중");
+        yield return new WaitForSeconds(1f); 
+        Game_Player.transform.position = Taget.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider Game_Player)
+    {
+        if(Game_Player.gameObject.tag == "Player")
+        {
+            Debug.Log("start");
+            StartCoroutine(waitTel());
+            Debug.Log("End");
+        }
+    }
+}
